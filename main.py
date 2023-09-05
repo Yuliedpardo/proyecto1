1f23699abff7e5a3c63a333ee19e8a8a7f992761
Yuliedpardo <129725138+Yuliedpardo@users.noreply.github.com>
2023-09-05 08:27:01 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -95,40 +95,63 @@ def countreviews(start_date: str, end_date: str):
 
 from fastapi.responses import JSONResponse
 
-@app.get('/developer/{year}')
-def developer(year):
-    try:
-        # Paso 1: Filtrar las filas para el año especificado
-        filtered_output = consulta5[consulta5['release_year'] == year]
+@app.get('/developer/{developer}')
+def developer(developer):
+    consulta5['release_year']=consulta5['release_year'].astype(str)
 
-        # Paso 2: Agrupar por desarrollador y calcular la cantidad total de ítems para cada uno
-        developer_items_count = filtered_output.groupby('developer')['item_id'].count().reset_index()
+    dataDeveloper = consulta5[consulta5['developer']== developer]
+    itemsPearYear = dataDeveloper.groupby('release_year')['item_id'].nunique()
+    
+    cantidadItems = dataDeveloper.groupby('developer')['item_id'].nunique()
+    
+    itemsFreePearYear = dataDeveloper[dataDeveloper['price']==0].groupby('release_year')['item_id'].nunique()
+    porcentaje= (itemsFreePearYear/itemsPearYear)*100
+    
+    rowPercent = []
+    for anio in itemsPearYear.index:
+        
+        percentFree = porcentaje.get(anio)
+        
+        if np.isnan(percentFree):        
+            rowPercent.append({'Año': anio,'Contenido Free':'0.00%'})
+        
+        else:
+            rowPercent.append({'Año': anio,'Contenido Free':f"{percentFree:.2f}%"})
 
-        # Paso 3: Calcular la cantidad de ítems con precio igual a 0 (contenido gratuito) por desarrollador
-        developer_free_items_count = filtered_output[filtered_output['price'] == 0].groupby('developer')['item_id'].count().reset_index()
+    return {'Cantidad Items': str(cantidadItems.iloc[0]),'Porcentaje Contenido Free':rowPercent}
+# def developer(year):
+#     try:
+#         # Paso 1: Filtrar las filas para el año especificado
+#         filtered_output = consulta5[consulta5['release_year'] == year]
 
-        # Paso 4: Fusionar los DataFrames para tener la información completa
-        developer_info = pd.merge(developer_items_count, developer_free_items_count, on='developer', how='left')
+#         # Paso 2: Agrupar por desarrollador y calcular la cantidad total de ítems para cada uno
+#         developer_items_count = filtered_output.groupby('developer')['item_id'].count().reset_index()
 
-        # Paso 5: Calcular el porcentaje de contenido gratuito y reemplazar los NaN con 0%
-        developer_info['Contenido Free'] = (developer_info['item_id_y'] / developer_info['item_id_x'] * 100).fillna(0).astype(int).astype(str) + '%'
+#         # Paso 3: Calcular la cantidad de ítems con precio igual a 0 (contenido gratuito) por desarrollador
+#         developer_free_items_count = filtered_output[filtered_output['price'] == 0].groupby('developer')['item_id'].count().reset_index()
 
-        # Paso 6: Renombrar las columnas para obtener el formato de salida deseado
-        developer_info.rename(columns={'developer': 'Empresa Desarrolladora', 'item_id_x': 'Cantidad de Ítems', 'item_id_y': 'Contenido Free'}, inplace=True)
+#         # Paso 4: Fusionar los DataFrames para tener la información completa
+#         developer_info = pd.merge(developer_items_count, developer_free_items_count, on='developer', how='left')
 
-        # Crear un diccionario con los resultados
-        result = {
-            "Año": year,
-            "Información de Desarrolladores": developer_info.to_dict()
-        }
-        return result
-        # Devolver el resultado en formato JSON usando JSONResponse
-        # return JSONResponse(content=result, status_code=200)
+#         # Paso 5: Calcular el porcentaje de contenido gratuito y reemplazar los NaN con 0%
+#         developer_info['Contenido Free'] = (developer_info['item_id_y'] / developer_info['item_id_x'] * 100).fillna(0).astype(int).astype(str) + '%'
+
+#         # Paso 6: Renombrar las columnas para obtener el formato de salida deseado
+#         developer_info.rename(columns={'developer': 'Empresa Desarrolladora', 'item_id_x': 'Cantidad de Ítems', 'item_id_y': 'Contenido Free'}, inplace=True)
+
+#         # Crear un diccionario con los resultados
+#         result = {
+#             "Año": year,
+#             "Información de Desarrolladores": developer_info.to_dict()
+#         }
+#         return result
+#         # Devolver el resultado en formato JSON usando JSONResponse
+#         # return JSONResponse(content=result, status_code=200)
        
-    except Exception as e:
-        # Si ocurre una excepción, devolver un mensaje de error en formato JSON con código de estado 500 (Error interno del servidor)
-        error_data = {'error': str(e)}
-        return JSONResponse(content=error_data, status_code=500)
+#     except Exception as e:
+#         # Si ocurre una excepción, devolver un mensaje de error en formato JSON con código de estado 500 (Error interno del servidor)
+#         error_data = {'error': str(e)}
+#         return JSONResponse(content=error_data, status_code=500)
 
     
 # @app.get('/developer/{year}')
